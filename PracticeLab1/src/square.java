9aea4993f20a62932b6bde43cbb9ec82069ec6ad
JOREEN DEEIN <JOREEN DEEIN@JoreenDeein-28.bbrouter>
2020-09-01 17:53:26 +0800

    first commit

---
+++ PracticeLab1/src/square.java
@@ -0,0 +1,25 @@
+//Problem Number 4
+public class square {
+
+	public static void main(String[] args) {
+		getSquare(23); 
+		getSquare2(5, 10); 
+	
+	}
+
+	static  void getSquare(int n) {
+		for(int i = 0; i < n || i ==n; i++) {
+			System.out.println("the square of " + i + " is " + i*i);
+		}
+	}
+	
+	static void getSquare2(int i, int n) {
+		if(i < n || i == n) {
+			System.out.println(i + " , " + i*i);
+			getSquare2(i + 1, n);
+		}else {
+			return;
+		}
+	}
+
+}
---
+++ PracticeLab1/src/test.java
@@ -0,0 +1,18 @@
+//Problem Number 1 
+
+import java.util.ArrayList;
+
+public class test {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> testInts = new ArrayList();
+		testInts.add(5);
+		testInts.add(10);
+		testInts.add(23);
+		testInts.add(28);
+		
+	System.out.println(Integers.computeSum(testInts));
+
+	}
+
+}
